fd22b4c5bec03955f2f0575bf8e00dfb84f179bb
liankong31 <2536145609@qq.com>
2019-03-16 23:27:11 +0800

    加注释

--- 编程题与分析/bind.js
+++ 编程题与分析/bind.js
@@ -1,4 +1,9 @@
 
+// 因为bind的使用方法是 某函数.bind(某对象，...剩余参数)
+// 所以需要在Function.prototype 上进行编程
+// 将传递的参数中的某对象和剩余参数使用apply的方式在一个回调函数中执行即可
+// 要在第一层获取到被绑定函数的this，因为要拿到那个函数用apply
+// /**
 console.log("下面是自封装的bind方法实现");
 Function.prototype.myBind=function(target){
 	target=target||window;
